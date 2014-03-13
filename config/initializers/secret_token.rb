00b686b6bb317b84899aa25033779110d14097da
K Mak <karen.mak@stx.oxon.org>
2014-03-13 15:31:32 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BigData::Application.config.secret_key_base = 'b88091bada6926a468689e1e55eb53488bbf30b042f9a8003ab84268c24354137505ec898dcc5e54a86da4494558dc2108a8bd4a3e2ee98102fd48ecc3c88380'
